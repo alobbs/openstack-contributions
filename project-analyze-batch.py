a4cfd0eba458a15eae6d6d34ca6a52cb4072a939
Alvaro Lopez Ortega <alvaro@alobbs.com>
2012-10-20 07:19:37 -0700

    Adds --days support to project-analyze-batch.

--- project-analyze-batch.py
+++ project-analyze-batch.py
@@ -14,6 +14,7 @@ import argparse
 # Argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument ('--use-cache', action="store_true", default=False, help="use a log cache")
+parser.add_argument ('--days',      action="store",      default=0,     help="evaluate only the last DAYS days", type=int)
 ns = parser.parse_args()
 
 def main():
@@ -21,8 +22,12 @@ def main():
     for repo in repos:
         # Build command
         cmd = "python project-analyze.py "
+
         if ns.use_cache:
             cmd += "--use-cache "
+        if ns.days:
+            cmd += "--days=%d " %(ns.days)
+
         cmd += repo
 
         # Execute it
