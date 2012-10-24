37bb34be57ee688a98b39ea75a05b7c7c27a1544
Alvaro Lopez Ortega <alvaro@alobbs.com>
2012-10-24 19:34:21 +0200

    Implements basic HTML reports generation

--- project-analyze.py
+++ project-analyze.py
@@ -9,48 +9,64 @@ import os
 import conf
 import time
 import pickle
+import json
 import argparse
 import projects
 import patches
 import companies
+import utils
+import releases
 
 
 # Argument parsing
 parser = argparse.ArgumentParser()
-parser.add_argument ('project',          action="store")
-parser.add_argument ('--days',           action="store",      default=0,     help="evaluate only the last DAYS days", type=int)
-parser.add_argument ('--use-cache',      action="store_true", default=False, help="use a log cache")
-parser.add_argument ('--print-unknowns', action="store_true", default=False, help="find commits by unknown companies")
+parser.add_argument ('--use-cache', action="store_true", default=False, help="use a log cache")
 ns = parser.parse_args()
 
 
-def get_commits():
-    cache_fp = os.path.join (conf.CACHE_PATH, ns.project + '-log.pickle')
-
-    # Cached
-    if ns.use_cache:
-        return pickle.load (open (cache_fp, 'r'))
-
+def parse_git_log (project):
     # Non-cached
     cmd = 'git log --no-merges \'--pretty=format:{"author":"%aN", "author_email":"%aE", "author_date": "%at", "committer":"%cN", "committer_email":"%cE", "committer_date": "%ct", "hash":"%H"},\''
-    f = projects.popen (ns.project, cmd)
+    f = projects.popen (project, cmd)
     commits = eval('[' + f.read() + ']')
     commitsn = len(commits)
+
+    # Fix the type of the time entries
+    for n in range(commitsn):
+        commits[n]['author_date']    = int(commits[n]['author_date'])
+        commits[n]['committer_date'] = int(commits[n]['committer_date'])
 
     # Populate patch sizes
     for n in range(commitsn):
         commit = commits[n]
 
         cmd = 'git show --no-notes %s' %(commit['hash'])
-        f = projects.popen (ns.project, cmd)
+        f = projects.popen (project, cmd)
 
         # commit['size'] = len(f.read())
         commit['size'] = len (patches.filter_contribution(f.read()))
         print '\r%d%% [%d/%d] %s size=%d%s' %(((n+1) * 100) / commitsn, n, commitsn, commit['hash'], commit['size'], ' '*10),
 
-    # Write cache file
-    pickle.dump (commits, open(cache_fp, 'w+'))
     return commits
+
+
+_commits = {}
+def get_commits (project):
+    # Cache
+    global _commits
+    if _commits.has_key (project):
+        return _commits[project]
+
+    cache_fp = os.path.join (conf.CACHE_PATH, project + '-log.pickle')
+
+    # Disk cache
+    if ns.use_cache:
+        _commits[project] = pickle.load (open (cache_fp, 'r'))
+    else:
+        _commits[project] = parse_git_log (project)
+        pickle.dump (_commits, open(cache_fp, 'w+'))
+
+    return _commits[project]
 
 
 def figure_out_company (commits):
@@ -60,68 +76,172 @@ def figure_out_company (commits):
     return commits
 
 
-def report_by_company (commits, companies):
-    report      = {}
-    global_size = sum ([c['size'] for c in commits])
+class Company_Analyzer:
+    def __init__ (self, company, repo, date_start=None, date_end=None):
+        self.name            = company
+        self.repo            = repo
+        self.commits_all     = figure_out_company (get_commits(repo))
+        self.date_start      = date_start or self.get_first_commit()
+        self.date_end        = date_end   or self.get_latest_commit()
+        self.commits_company = [c for c in self.commits_all if c.get('company') == company]
+        self.commits         = self._get_filtered_commits_by_date (self.commits_company,
+                                                                   self.date_start,
+                                                                   self.date_end)
 
-    for company in companies:
-        company_name    = company or 'unknown'
-        company_commits = [c for c in commits if c.get('company') == company]
+    def get_first_commit (self):
+        return min([c['author_date'] for c in self.commits_all])
 
-        # Commits number
-        commits_num     = len(company_commits)
-        commits_percent = len(company_commits) * 100.0 / len(commits)
+    def get_latest_commit (self):
+        return max([c['author_date'] for c in self.commits_all])
 
-        # Commits size
-        size_total   = sum ([x['size'] for x in company_commits])
-        size_percent = size_total * 100.0 / global_size
+    def _get_filtered_commits_by_date (self, commits, d1, d2):
+        return [c for c in commits if (c['author_date'] >= d1 and
+                                       c['author_date'] <= d2)]
 
-        # Average percentage
-        average_percent = (commits_percent * 0.8) + (size_percent * 0.2)
-
-        # Company entry
-        report[company_name] = {'commits_num':     commits_num,
-                                'commits_percent': commits_percent,
-                                'size_total':      size_total,
-                                'size_percent':    size_percent,
-                                'average_percent': average_percent}
-
-    # Calculate the ranking
-    ranking = sorted (report.keys(), key = lambda x: report[x]['average_percent'], reverse=True)
-
-    for company in companies:
-        company_name = company or 'unknown'
-        report[company_name]['rank'] = ranking.index(company_name) + 1
-
-    # Save pickle
-    cache_fp = os.path.join (conf.CACHE_PATH, ns.project + '-report.pickle')
-    pickle.dump (report, open(cache_fp, 'w+'))
-
-    return report
+    def _get_authors_dict (self, commits):
+        authors = {}
+        for author in list(set([x['author'] for x in commits])):
+            authors[author] = len([c for c in commits if c['author'] == author])
+        return authors
 
 
-def commits_filter_by_date (commits, days):
-    date_last   = time.time() - (days * 24 * 60 * 60)
-    new_commits = [c for c in commits if int(c['author_date']) > date_last]
+    def run (self):
+        raise NotImplementedError ('You must implement run()')
 
-    print ("Evaluating %d commits out of %d" %(len(new_commits), len(commits)))
-    return new_commits
+
+class Company_Report:
+    def __init__ (self, name):
+        self.name               = name
+        self.slices             = []
+        self.total_commits_num  = 0
+        self.total_commits_size = 0
+
+
+class Company_Analyzer_by_date (Company_Analyzer):
+    DEFAULT_LAPSE = 7*24*60*60
+
+    def __init__ (self, project, company, date_start=None, date_end=None, lapse=None):
+        Company_Analyzer.__init__ (self, company, project, date_start, date_end)
+        self.lapse = lapse or self.DEFAULT_LAPSE
+
+    def _get_time_points (self):
+        tps = range (self.date_start, self.date_end, self.lapse)
+
+        # The last point in time might not be the last one of the
+        # list, but odds are we want it represented as well.
+        if tps[-1] < self.date_end:
+            tps.append (self.date_end)
+
+        return tps
+
+    def _analyze_time_slice (self, commits, time):
+        report_slice = {}
+        report_slice['commits_num']  = len(commits)
+        report_slice['commits_size'] = sum ([x['size'] for x in commits])
+        report_slice['authors']      = self._get_authors_dict (commits)
+        report_slice['time']         = time
+        return report_slice
+
+    def _analyze (self):
+        # Report
+        report = Company_Report (self.name)
+
+        # Times sliced analysis
+        t1 = None
+        for t2 in self._get_time_points():
+            if not t1:
+                t1 = t2
+                continue
+
+            commits = self._get_filtered_commits_by_date (self.commits, t1, t2)
+            report_slice = self._analyze_time_slice (commits, (t2+t1)/2)
+            report.slices.append (report_slice)
+            t1 = t2
+
+        # Compute totals
+        for s in report.slices:
+            report.total_commits_num  += s['commits_num']
+            report.total_commits_size += s['commits_size']
+
+        return report
+
+    def run (self):
+        self.report = self._analyze()
+
+
+class HTML_Report_Period_Commits():
+    def __init__ (self, project, company_names=None, *args, **kwargs):
+        self.project   = project
+        self.companies = company_names or companies.KNOWN
+        self.args      = args
+        self.kwargs    = kwargs
+        self.report    = {}
+
+    # JSON
+    #
+    def _get_JSON_property_company (self, key, company):
+        # Data
+        row = []
+        for report_slice in self.report[company].slices:
+            row.append ([report_slice['time'], report_slice[key]])
+
+        # Total amount
+        total = sum([s[key] for s in self.report[company].slices])
+
+        return {"label": company, "data": row, "total": total}
+
+    def _get_JSON_property_all_companies (self, key):
+        # Companies data
+        companies_jsons = []
+
+        for company in self.report:
+            companies_jsons.append (self._get_JSON_property_company (key, company))
+
+        companies_jsons = sorted (companies_jsons, key=lambda cjson: cjson["total"], reverse=True)
+
+        # Highest value - to normalize graphs
+        highest_global = 0
+        for company in self.report:
+            tmp = max ([s[key] for s in self.report[company].slices])
+            highest_global = max (highest_global, tmp)
+
+        return {"info": companies_jsons, "highest_value": highest_global}
+
+    def _get_JSON_all_companies (self):
+        return {'commits_num':  self._get_JSON_property_all_companies ('commits_num'),
+                'commits_size': self._get_JSON_property_all_companies ('commits_size')}
+
+    def get_JSON (self):
+        # Generate individual reports for the companies
+        for company in self.companies:
+            analyzer = Company_Analyzer_by_date (self.project, company, *self.args, **self.kwargs)
+            analyzer.run()
+            self.report[company] = analyzer.report
+
+        # Print the results
+        obj = self._get_JSON_all_companies()
+        return json.dumps (obj)
+
+
+def generate_release_commits_HTML_report():
+    for r in releases.releases:
+        for proj_name in r['projects']:
+            # Generate report
+            report = HTML_Report_Period_Commits (proj_name,
+                                                 date_start = r["period"][0],
+                                                 date_end   = r["period"][1]);
+
+            # Write JSON file
+            report_name = '%s-%s.js' %(proj_name, r['name'].lower())
+            report_path = os.path.join ("web", report_name)
+
+            print ("Writing %s..." %(report_path))
+            with open(report_path, 'w+') as f:
+                f.write (report.get_JSON())
 
 
 def main():
-    commits = get_commits()
-    commits = figure_out_company (commits)
-
-    if ns.days:
-        commits = commits_filter_by_date (commits, ns.days)
-
-    if ns.print_unknowns:
-        print '\n'.join ([str(c) for c in commits if c.get('company') == None])
-        raise SystemExit
-
-
-    report = report_by_company (commits, companies.KNOWN + [None])
-    print "\n%s: Analisys finished" %(ns.project)
+    generate_release_commits_HTML_report()
 
 
 if __name__ == "__main__":
---
+++ web/project-release.js
@@ -0,0 +1,149 @@
+function getURLParameter (name)
+{
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
+
+function capitalise (string)
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function addCompanyReports (data)
+{
+    for (cn in data.commits_num.info) {
+	   commits_num  = data.commits_num;
+	   commits_size = data.commits_size;
+
+	   company_data_num  = commits_num.info[cn];
+	   company_data_size = commits_size.info[cn];
+
+	   /* Company List */
+	   if (company_data_num.total <=0) {
+		  $("#lazy_fellows").append('<li>'+ capitalise(company_data_num.label) +'</li>');
+		  continue;
+	   }
+
+	   /* Graphs placeholders */
+	   id_graph      = "graph_"+company_data_num.label.replace(/[^a-z]+/gi,'');
+	   id_graph_num  = id_graph + "_num";
+	   id_graph_size = id_graph + "_size";
+
+	   $('#graphs').append ("<div id='"+id_graph+"'>" +
+					      "<div id='"+id_graph_num+"'></div>" +
+					      "<div id='"+id_graph_size+"'></div>" +
+					    "</div>");
+
+	   $('#'+id_graph_num).css ('width',  '800px');
+	   $('#'+id_graph_num).css ('height', '150px');
+	   $('#'+id_graph_size).css('width',  '800px');
+	   $('#'+id_graph_size).css('height', '145px');
+
+	   /* Graph: number */
+	   Flotr.draw (document.getElementById(id_graph_num), company_data_num,
+				{title: company_data_num.label,
+				 bars: {
+					show: true,
+					barWidth: 0.8,
+					lineWidth: 10,
+				 },
+				 xaxis : { showLabels: false },
+				 yaxis : {
+					min: 0,
+					max: commits_num.highest_value,
+					tickDecimals: 0,
+				 },
+				 grid : {
+					verticalLines : false,
+					horizontalLines : true,
+				 },
+				 legend : {
+					position : 'ne',
+					noColumns: 3,
+					margin: 300,
+				 }
+				});
+
+	   /* Graph: Size */
+	   Flotr.draw (document.getElementById(id_graph_size), company_data_size,
+				{bars: {
+				    show: true,
+				    barWidth: 0.8,
+				    lineWidth: 10,
+				 },
+				 xaxis : {
+				 	tickFormatter: function (num) {
+				 	    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+				 	    date = new Date(num * 1000);
+				 	    return months[date.getMonth()];
+				 	}
+				 },
+				 yaxis : {
+				 	tickFormatter: function (num) {
+					    return Math.round(num / 1024) + "Kb";
+
+
+				 	    date = new Date(num * 1000);
+				 	    return months[date.getMonth()];
+				 	},
+					min: 0,
+					max: commits_size.highest_value,
+					tickDecimals: 0,
+				 },
+				 grid : {
+					verticalLines : false,
+					horizontalLines : true,
+				 },
+				 legend : {
+					position : 'ne',
+					noColumns: 3,
+					margin: 300,
+				 }
+				});
+
+	   /* Contributors lists */
+	   var caption    = capitalise(company_data_num.label) +' ('+ company_data_num.total +')';
+	   var control_id = "control_" + id_graph;
+	   var len        = $("#contributors_top5").find("li").length;
+
+	   if (len < 5) {
+		  $('#contributors_top5').append('<li><label id="'+control_id+'"><input type="checkbox" checked/>'+caption+'</label></li>');
+	   } else {
+		  $('#contributors').append('<li><label id="'+control_id+'"><input type="checkbox"/>'+caption+'</label></li>');
+		  $('#'+id_graph).hide()
+	   }
+
+	   $('#' + control_id).change (function() {
+		  var checked  = $(this).find('input').is(':checked');
+		  var graph_id = $(this).attr('id').replace('control_','');
+
+		  if (checked) {
+			 $('#'+graph_id).show();
+		  } else {
+			 $('#'+graph_id).hide();
+		  }
+	   });
+    }
+}
+
+
+$(function() {
+    var project = getURLParameter("project").replace(/[^a-z]+/gi,'');
+    var release = getURLParameter("release").replace(/[^a-z]+/gi,'');
+
+    if (!project || !release)
+	   return;
+
+    $("h1").html(capitalise(release) +' : '+ capitalise(project));
+
+    $.ajax({
+	   url:       project+'-'+release+'.js',
+	   dataType: 'json',
+	   error:     function (e) {console.log(e);},
+	   success:   function (data)
+	   {
+		  addCompanyReports (data);
+	   }
+    });
+});
